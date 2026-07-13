35a44dc8d24a340bd5e4d5dd6d31b196d60071c1
Ivan Bardarov <ivanbardarov73@gmail.com>
2026-07-13 22:27:29 +0300

    Add Tailwind CSS styling and update LoginPage.tsx CSS styling

--- plando-client/src/pages/LoginPage.tsx
+++ plando-client/src/pages/LoginPage.tsx
@@ -25,13 +25,20 @@ export const LoginPage = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}
+            className="flex flex-col items-center justify-center min-h-screen">
             <h1>Login</h1>
             <label>User</label>
-            <input type="email" onChange={e => setEmail(e.target.value)}/>
+            <input 
+                className="border rounded p-2 w-full"
+                type="email" onChange={e => setEmail(e.target.value)}/>
             <label>Password</label>
-            <input type="password" onChange={e => setPassword(e.target.value)}/>
-            <button type="submit">Login</button>
+            <input 
+                className="border rounded p-2 w-full"
+                type="password" onChange={e => setPassword(e.target.value)}/>
+            <button 
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+                type="submit">Login</button>
         </form>
     );
 };
