88ae8fa725de5ae2b03b00c1c28c0e2cb2a5e82f
Ivan Bardarov <ivanbardarov73@gmail.com>
2026-07-14 22:55:24 +0300

    Add Query tests for GetTaskItemsByUserIdQueryHandler

---
+++ Plando.Tests/Queries/GetTaskItemsByUserIdQueryHandlerTests.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Moq;
+using Plando.Application.DTOs;
+using Plando.Application.Interfaces;
+using Plando.Application.Queries.TaskItems;
+using Plando.Domain.Entities;
+
+namespace Plando.Tests.Queries;
+
+public class GetTaskItemsByUserIdQueryHandlerTests
+{
+    private readonly Mock<ITaskItemRepository> _taskItemRepositoryMock;
+    private readonly GetTaskItemsByUserIdQueryHandler _handler;
+
+    public GetTaskItemsByUserIdQueryHandlerTests()
+    {
+        _taskItemRepositoryMock = new Mock<ITaskItemRepository>();
+
+        _handler = new GetTaskItemsByUserIdQueryHandler(
+            _taskItemRepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task ReturnsAllTaskItems()
+    {
+        var passwordHasher = new PasswordHasher<string>();
+        var realHash = passwordHasher.HashPassword("test@example.com", "abc123");
+        var user = User.Create("test@example.com", realHash);
+
+        var taskItemsList = new List<TaskItem>();
+        var taskItemDTOsList = new List<TaskItemDto>();
+
+        for (var i = 1; i < 4; i++)
+        {
+            var taskItem = TaskItem.Create(
+                $"Title{i}",
+                $"Description{i}",
+                DateTime.UtcNow,
+                user,
+                null);
+
+            var taskItemDto = TaskItemDto.FromEntity(taskItem);
+
+            taskItemsList.Add(taskItem);
+            taskItemDTOsList.Add(taskItemDto);
+        }
+
+        _taskItemRepositoryMock
+            .Setup(s => s.GetAllByUserIdAsync(user.Id))
+            .ReturnsAsync(taskItemsList);
+
+        var query = new GetTaskItemsByUserIdQuery(
+            user.Id, null, null, null, null, null,
+            null, null, null, null);
+
+        var result = await _handler.HandleAsync(query);
+
+        Assert.Equal(taskItemDTOsList.Count, result.Count());
+        Assert.Equal(taskItemDTOsList.Select(t => t.Title),
+            result.Select(t => t.Title));
+        Assert.Equal(taskItemDTOsList.Select(t => t.Description),
+            result.Select(t => t.Description));
+    }
+
+    [Fact]
+    public async Task FilterByTitle()
+    {
+        var passwordHasher = new PasswordHasher<string>();
+        var realHash = passwordHasher.HashPassword("test@example.com", "abc123");
+        var user = User.Create("test@example.com", realHash);
+
+        var taskItemsList = new List<TaskItem>();
+        var taskItemDTOsList = new List<TaskItemDto>();
+
+        for (var i = 1; i < 11; i++)
+        {
+            var prefix = (i % 2 == 0) ? "Title" : "Name";
+
+            var taskItem = TaskItem.Create(
+                $"{prefix}{i}",
+                $"Description{i}",
+                DateTime.UtcNow,
+                user,
+                null);
+
+            var taskItemDto = TaskItemDto.FromEntity(taskItem);
+
+            taskItemsList.Add(taskItem);
+
+            if (taskItem.Title.Contains("Title"))
+                taskItemDTOsList.Add(taskItemDto);
+        }
+
+        _taskItemRepositoryMock
+            .Setup(s => s.GetAllByUserIdAsync(user.Id))
+            .ReturnsAsync(taskItemsList);
+
+        var query = new GetTaskItemsByUserIdQuery(
+            user.Id, "Title", null, null, null, null,
+            null, null, null, null);
+
+        var result = await _handler.HandleAsync(query);
+
+        Assert.Equal(taskItemDTOsList.Count, result.Count());
+        Assert.Equal(taskItemDTOsList.Select(t => t.Title),
+            result.Select(t => t.Title));
+    }
+
+    [Fact]
+    public async Task FilterByIsCompleted()
+    {
+        var passwordHasher = new PasswordHasher<string>();
+        var realHash = passwordHasher.HashPassword("test@example.com", "abc123");
+        var user = User.Create("test@example.com", realHash);
+
+        var taskItemsList = new List<TaskItem>();
+        var taskItemDTOsList = new List<TaskItemDto>();
+
+        for (var i = 1; i < 11; i++)
+        {
+            var taskItem = TaskItem.Create(
+                $"Title{i}",
+                $"Description{i}",
+                DateTime.UtcNow,
+                user,
+                null);
+
+            if (i % 2 == 0)
+                taskItem.Complete();
+
+            var taskItemDto = TaskItemDto.FromEntity(taskItem);
+
+            taskItemsList.Add(taskItem);
+
+            if (taskItem.IsCompleted)
+                taskItemDTOsList.Add(taskItemDto);
+        }
+
+        _taskItemRepositoryMock
+            .Setup(s => s.GetAllByUserIdAsync(user.Id))
+            .ReturnsAsync(taskItemsList);
+
+        var query = new GetTaskItemsByUserIdQuery(
+            user.Id, null, null, null, null, null,
+            null, null, null, true);
+
+        var result = await _handler.HandleAsync(query);
+
+        Assert.Equal(taskItemDTOsList.Count, result.Count());
+        Assert.Equal(taskItemDTOsList.Select(t => t.Title),
+            result.Select(t => t.Title));
+        Assert.Equal(taskItemDTOsList.Select(t => t.IsCompleted),
+            result.Select(t => t.IsCompleted));
+    }
+
+    [Fact]
+    public async Task ReturnsEmptyList()
+    {
+        _taskItemRepositoryMock
+            .Setup(s => s.GetAllByUserIdAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(new List<TaskItem>());
+
+        var query = new GetTaskItemsByUserIdQuery(
+            Guid.NewGuid(), null, null, null, null, null,
+            null, null, null, true);
+
+        var result = await _handler.HandleAsync(query);
+
+        Assert.Empty(result);
+    }
+}
