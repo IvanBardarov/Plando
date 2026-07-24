663b5b680ff4bfc9ae693d35e1012a4f1daca3fb
Ivan Bardarov <ivanbardarov73@gmail.com>
2026-07-24 17:02:46 +0300

    Fix CreateTaskItemCommandHandler to save TaskListId

--- Plando.Application/Commands/TaskItems/CreateTaskItemCommandHandler.cs
+++ Plando.Application/Commands/TaskItems/CreateTaskItemCommandHandler.cs
@@ -8,24 +8,30 @@ namespace Plando.Application.Commands.TaskItems;
 public class CreateTaskItemCommandHandler
 {
     private readonly ITaskItemRepository _taskItemRepository;
+    private readonly ITaskListRepository _taskListRepository;
     private readonly IUserRepository _userRepository;
 
     public CreateTaskItemCommandHandler(ITaskItemRepository taskItemRepository,
-        IUserRepository userRepository)
+        IUserRepository userRepository, ITaskListRepository taskListRepository)
     {
         _taskItemRepository = taskItemRepository;
         _userRepository = userRepository;
+        _taskListRepository = taskListRepository;
     }
 
     public async Task<TaskItemDto> HandleAsync(CreateTaskItemCommand command)
     {
         var user = await _userRepository.GetByIdAsync(command.UserId);
 
+        TaskList? taskList = null;
+        if(command.TaskListId is not null)
+            taskList = await _taskListRepository.GetByIdAsync(command.TaskListId.Value);
+
         if (user is null)
             throw new DomainException("There is not such an user!");
 
         var taskItem = TaskItem.Create(command.Title, command.Description,
-            command.DueDate, user, command.StartDate);
+            command.DueDate, user, command.StartDate, taskList);
 
         await _taskItemRepository.AddAsync(taskItem);
         await _taskItemRepository.SaveChangesAsync();
