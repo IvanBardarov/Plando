11b0e0816e48938c99152a06714e42006b1ce435
Ivan Bardarov <ivanbardarov73@gmail.com>
2026-09-03 21:52:22 +0300

    feat: add active and overdue task containers in DayView

--- plando-client/src/components/calendar/DayView.tsx
+++ plando-client/src/components/calendar/DayView.tsx
@@ -11,7 +11,8 @@ export const DayView = (
 
     const hours = Array.from({ length: 25 }, (_, i) => i);
 
-    const [taskItems, setTaskItems] = useState<TaskItem[] | null>(null);
+    const [activeTaskItems, setActiveTaskItems] = useState<TaskItem[] | null>(null);
+    const [overdueTaskItems, setOverdueTaskItems] = useState<TaskItem[] | null>(null);
     const [taskItemSchedules, setTaskItemSchedules] = useState<TaskItemSchedule[] | null>(null);
 
     useEffect(() => {
@@ -19,13 +20,23 @@ export const DayView = (
             const yearStr = date.getFullYear();
             const monthStr = String(date.getMonth() + 1).padStart(2, '0');
             const dayStr = String(date.getDate()).padStart(2, '0');
+
             const dateStr = `${yearStr}-${monthStr}-${dayStr}`;
             const dateISO = new Date(dateStr);
+
             console.log(dateStr);
+
             const tasks =
                 await getTaskItemsUpToDateByUserId(dateISO, false);
-            setTaskItems(tasks);
+
+            const activeTasks = tasks?.filter(t => new Date(t.dueDate) >= date) ?? null;
+            setActiveTaskItems(activeTasks);
+
+            const overdueTasks = tasks?.filter(t => new Date(t.dueDate) < date) ?? null;
+            setOverdueTaskItems(overdueTasks);
+
             console.log(tasks);
+
             const taskSchedules = await getTaskItemSchedulesByDate(dateISO);
             setTaskItemSchedules(taskSchedules);
         };
@@ -37,16 +48,39 @@ export const DayView = (
 
             <div className="font-bold text-center">{y}-{m}-{d}</div>
 
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-2" style={{ height: '85vh' }}>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-2"
+                style={{ height: '85vh' }}>
 
-                <div className="border col-span-1 overflow-y-scroll">
-                    {taskItems?.map(t => (
-                        <div className="border bg-red-200 p-1 m-1"
-                            key={t.id}>
-                            {t.title} - {t.startDate != null ? new Date(t.startDate).toLocaleDateString()
-                                : new Date(t.createdAt).toLocaleDateString()}
+                <div className="col-span-1 flex flex-col h-full">
+
+                    <div className="border h-[42.5vh] overflow-hidden">
+                        <span className="m-1 p-1 bg-gray-600 text-white">Active Tasks</span>
+                        <div className="h-[40vh] overflow-y-scroll">
+                            {activeTaskItems?.map(t => (
+                                <div className="border-2 border-gray-400 p-1 m-1"
+                                    key={t.id}>
+                                    {t.title} - {t.startDate != null
+                                        ? new Date(t.startDate).toLocaleDateString()
+                                        : new Date(t.createdAt).toLocaleDateString()}
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
+
+                    <div className="border h-[42.5vh] overflow-hidden">
+                        <span className="m-1 p-1 bg-red-600 text-white">Overdue Tasks</span>
+                        <div className="h-[40vh] overflow-y-scroll">
+                            {overdueTaskItems?.map(t => (
+                                <div className="border-2 border-red-400 p-1 m-1"
+                                    key={t.id}>
+                                    {t.title} - {t.startDate != null
+                                        ? new Date(t.startDate).toLocaleDateString()
+                                        : new Date(t.createdAt).toLocaleDateString()}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
                 </div>
 
                 <div className="border col-span-3 overflow-y-scroll"
@@ -56,7 +90,7 @@ export const DayView = (
                         top: 0,
                         left: '48px',
                         width: '1px',
-                        height: '1440px',
+                        height: '1460px',
                         backgroundColor: '#dadce0'
                     }} />
                     {hours.map(h => (
